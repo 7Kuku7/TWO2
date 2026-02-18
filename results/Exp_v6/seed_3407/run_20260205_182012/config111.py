34e2095347febfd6fa1395de835feb3a69d05f22
7Kuku7 <121493421+7Kuku7@users.noreply.github.com>
2026-02-18 10:46:51 +0800

    Add files via upload

---
+++ results/Exp_v6/seed_3407/run_20260205_182012/config111.py
@@ -0,0 +1,49 @@
+# config.py
+import os
+
+class Config:
+    # ================= 路径设置 =================
+    # 数据集根目录
+    ROOT_DIR = "/home/abc/wq/TWO1/renders"
+    # MOS 标签文件
+    MOS_FILE = "/home/abc/wq/TWO1/mos_advanced.json"
+    # 结果保存主目录
+    OUTPUT_DIR = "results"
+    
+    # ================= 实验名称与描述 =================
+    EXP_NAME = "Exp_v6"  # 实验组名
+    DESCRIPTION = "多任务感知头消融实验"
+
+    # ================= 训练超参数 =================
+    SEED = 3407             # 随机种子，设置为 None 则随机
+    GPU_ID = "1"          # 指定 GPU
+    BATCH_SIZE = 4
+    NUM_WORKERS = 4
+    EPOCHS = 50
+    LR = 1e-4             # 学习率
+    
+    # ================= 损失函数权重 (Ablation Control) =================
+    # 通过设置权重为 0.0 来实现消融实验 (Ablation)
+    LAMBDA_MSE = 1.0      # 主分数回归损失
+    LAMBDA_RANK = 0.1     # 排序损失
+    LAMBDA_MI = 0.0       # 解耦损失 (MI Loss) -> 设为0即为 w/o Decoupling
+    LAMBDA_SUB = 0.0      # 子任务分数损失 -> 设为0即为 w/o Multi-task
+    LAMBDA_SSL = 0.2      # 自监督一致性损失 -> 设为0即为 w/o SSL
+
+    # ================= 模型配置 =================
+    NUM_FRAMES = 16        # 每个视频采样的帧数
+    USE_SUBSCORES = True  # 是否使用子分数头
+    USE_FUSION = False     # 是否使用特征融合模块
+    
+    # ================= 功能开关 =================
+    ENABLE_WANDB = False        # 是否使用 WandB 记录
+    WANDB_PROJECT = "OF-NeRF-QA"
+    SAVE_PER_VIDEO_RESULT = True # 是否保存每个视频的预测结果JSON
+
+    @classmethod
+    def get_output_path(cls):
+        """自动生成当前实验的保存路径"""
+        path = os.path.join(cls.OUTPUT_DIR, cls.EXP_NAME, f"seed_{cls.SEED}")
+        os.makedirs(path, exist_ok=True)
+        return path
+
